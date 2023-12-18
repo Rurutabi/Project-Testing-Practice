1a6492aaa7e66b620f5caa1586a26694c3dd5d65
Sira <siraphop_2538@hotmail.com>
2023-12-18 16:58:19 +1100

    more handle case/test to capitalize

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,7 +1,9 @@
 const capitalize = function (someString) {
+  if (someString === "") return someString;
+
+  if (someString[0] >= "0" && someString[0] <= "9") return someString;
+
   return someString[0].toUpperCase() + someString.slice(1, someString.length);
 };
-
-console.log(capitalize("hello"));
 
 module.exports = capitalize;
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,6 +1,10 @@
 const capitalize = require("../code/capitalize");
 
 describe("Rerturn with first character capitalized.", function () {
+  test('with an empty string"', function () {
+    expect(capitalize("")).toEqual("");
+  });
+
   test('with one letter"', function () {
     expect(capitalize("h")).toEqual("H");
   });
@@ -11,5 +15,9 @@ describe("Rerturn with first character capitalized.", function () {
 
   test('with mutiple letters but only the first character capitalized"', function () {
     expect(capitalize("hello")).toEqual("Hello");
+  });
+
+  test('with a string starting with a non-alphabetic character"', function () {
+    expect(capitalize("123abc")).toEqual("123abc");
   });
 });
